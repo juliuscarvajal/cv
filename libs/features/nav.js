eb3722ff6e31c5c0e3f46ddf31b9c3a0ff68ca83
f00 <juliuscarvajal21@gmail.com>
2024-08-28 15:33:30 +1000

    Update nav.js

--- libs/features/nav.js
+++ libs/features/nav.js
@@ -1,7 +1,7 @@
 var link = document.querySelector('link[rel="import"]');
 
-    // Clone the <template> in the import.
-    var template = link.import.querySelector('template');
-    var clone = document.importNode(template.content, true);
+// Clone the <template> in the import.
+var template = link.import.querySelector('template');
+var clone = document.importNode(template.content, true);
 
-    document.querySelector('#nav').appendChild(clone);
+document.querySelector('#nav').appendChild(clone);
